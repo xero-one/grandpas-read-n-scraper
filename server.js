e54d13371303f017d539835b87de38412eea69dc
Chad Gresham <bxxdybagz@gmail.com>
2019-11-19 20:06:59 -0500

    bulding html, css and more elements, build build build

--- server.js
+++ server.js
@@ -1,36 +1,53 @@
+/*Set constant variable for npm express*/
 const express = require("express");
 
-/*Set constant variable for npm mongoose*/
-const mongoose = require("mongoose");
+/*Set constant variable for npm express-handlebars*/
+const exphbs = require("express-handlebars")
 
-const routes = require("./routes");
+/*Set constant variable for npm mongoose db*/
+const mongoose = require('mongoose');
 
-
-const PORT = process.env.PORT || 3306;
-
-/*Global constant to call the function npm express to initialize*/
+/*Set constant to call the function npm express to initialize*/
 const app = express();
 
+/*Set constant variable to set npm express router protocal*/
+const router = express.Router();
 
-/*Connect to the Mongo DB*/
-const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/scraper";
+/*Set constant to db to equal deployed database or use default local mongoHeadlines database*/
+const db = proess.env.MONGODB_URI || "mongodb://localhost/articleHeadlines";
 
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
+/*Call mongoose to connect to the server and run log command status */
+mongoose.connect(db, function(err) {
+    if (err) {
+        console.log(err);
+    }
+    else {
+        console.log("Mongoose has successfully connected!");
+    }
+});
 
-app.use(express.static("public"));
+/*Set a constant variable for your port or app port*/
+const PORT = process.env.PORT || 3306;
 
+/*Call the app to label the public folder as static*/
+app.use(express.static(__dirname + "/public"));
 
-app.use("/",routes);
+/*Set the app to connect handlebars with express*/
+app.engine("handlebars", exphbs({
+    defaultLayout: + "main"
+  }));
 
+app.get('/', function (req, res) {
+    res.render('home');
+});
+ 
+/*Call the app to use router middleware for every request*/
+app.use(router);
 
-mongoose.connect(MONGODB_URI, { useNewUrlParser: true })
-  .then((data) => {
-    /*Add listener function that calls the port*/
-    app.listen(PORT, function () {
-      console.log("Scraper running on port " + PORT + "!");
-    });
-})
-.catch(err => {
-  console.log('There is an error connecting to MongoDB');
+/*Set the app to use han*/
+app.set("view engine", "handlebars");
+
+/*Set the app to listen to PORT set above*/
+app.listen(PORT, function() {
+    console.log("The app is listening om port: " + PORT);
 });
